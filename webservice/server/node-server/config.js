dcb1bc551dfabcd225e52a26c6d735c119b086b1
Jarnen Richard <richard.jarnen@gmail.com>
2020-04-28 22:43:16 +0800

    Change http port 3002 to 3000

--- webservice/server/node-server/config.js
+++ webservice/server/node-server/config.js
@@ -2,6 +2,6 @@ module.exports = {
     mqtt: {
         port: 3000,
         host: 'localhost',
-        http: {port: 3002, bundle: true, static: './'}  
+        http: {port: 3000, bundle: true, static: './'}  
     }
 }
